4231ee1fcc51093529a30aef589ce5040326ad33
zcxxv1213 <403819524@qq.com>
2018-07-20 14:35:34 +0800

    remove

--- Assets/Scripts/Com/Game/Enum/SceneEnum.cs
+++ Assets/Scripts/Com/Game/Enum/SceneEnum.cs
@@ -5,16 +5,10 @@ using System.Text;
 
 namespace Assets.Scripts.Com.Game.Enum
 {
-    /// <summary>
-    /// 场景类型
-    /// </summary>
     public enum SceneEnum
     {
         RAW = 0,
         MAIN,
-        DUNGEON,
-        MELEE,
-        ARENA,
-        MELEE_DUNGEON,
+     
     }
 }
