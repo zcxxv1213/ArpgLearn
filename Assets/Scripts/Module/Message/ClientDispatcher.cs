f6c150ed24afe847c9d1f62df7b4e1ae9ee4a2a6
zcxxv1213 <403819524@qq.com>
2018-08-10 15:01:27 +0800

    Commit

--- Assets/Scripts/Module/Message/ClientDispatcher.cs
+++ Assets/Scripts/Module/Message/ClientDispatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace ETModel
 {
@@ -6,6 +7,7 @@ namespace ETModel
 	{
 		public void Dispatch(Session session, Packet packet)
 		{
+            Debug.Log(packet.Opcode);
 			object message;
 			try
 			{
---
+++ Assets/Scripts/SyncFrameWork/Handle/M2C_CreatUnit_Handle.cs
@@ -0,0 +1,21 @@
+﻿using ETModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.SyncFrameWork.Handle
+{
+    [MessageHandler(AppType.Map)]
+    public class M2C_CreatUnit_Handle : AMHandler<Actor_CreateUnits>
+    {
+        protected override void Run(Session session, Actor_CreateUnits message)
+        {
+            Debug.Log("RevMsg");
+            Debug.Log(message.ActorId);
+            Debug.Log(message.Units[0].UnitId);
+        }
+    }
+}
