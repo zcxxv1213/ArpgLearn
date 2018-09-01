1985787a3abf71b7b1c33f676692704fa80f6423
zcxxv1213 <403819524@qq.com>
2018-09-01 15:59:50 +0800

    XJB再提交

--- Assets/Scripts/Module/Message/Hotfix/FrameOpcode.cs
+++ Assets/Scripts/Module/Message/Hotfix/FrameOpcode.cs
@@ -4,12 +4,6 @@ namespace ETModel
 	[Message(FrameOpcode.OneFrameMessage)]
 	public partial class OneFrameMessage : IActorMessage {}
 
-	[Message(FrameOpcode.InputState)]
-	public partial class InputState {}
-
-	[Message(FrameOpcode.InputFormat)]
-	public partial class InputFormat {}
-
 	[Message(FrameOpcode.FrameMessage)]
 	public partial class FrameMessage : IActorMessage {}
 
@@ -44,16 +38,14 @@ namespace ETModel
 	public static partial class FrameOpcode
 	{
 		 public const ushort OneFrameMessage = 20001;
-		 public const ushort InputState = 20002;
-		 public const ushort InputFormat = 20003;
-		 public const ushort FrameMessage = 20004;
-		 public const ushort LocalNCFAndJLE = 20005;
-		 public const ushort JoinEvent = 20006;
-		 public const ushort JoinLeaveEvent = 20007;
-		 public const ushort OnlineState = 20008;
-		 public const ushort InputRLE = 20009;
-		 public const ushort InputCoalesced = 20010;
-		 public const ushort InputHeader = 20011;
-		 public const ushort InputPredictionWarmValues = 20012;
+		 public const ushort FrameMessage = 20002;
+		 public const ushort LocalNCFAndJLE = 20003;
+		 public const ushort JoinEvent = 20004;
+		 public const ushort JoinLeaveEvent = 20005;
+		 public const ushort OnlineState = 20006;
+		 public const ushort InputRLE = 20007;
+		 public const ushort InputCoalesced = 20008;
+		 public const ushort InputHeader = 20009;
+		 public const ushort InputPredictionWarmValues = 20010;
 	}
 }
