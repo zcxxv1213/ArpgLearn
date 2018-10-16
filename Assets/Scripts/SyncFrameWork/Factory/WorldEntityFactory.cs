553833ed4271a691054d66a7b43c2807a3d32a42
zcxxv1213 <403819524@qq.com>
2018-10-16 16:55:36 +0800

    恶心心闪退BUG

--- Assets/Scripts/SyncFrameWork/Factory/WorldEntityFactory.cs
+++ Assets/Scripts/SyncFrameWork/Factory/WorldEntityFactory.cs
@@ -8,7 +8,13 @@ namespace ETModel {
         public static WorldEntity Create()
         {
             WorldManagerComponent worldManagerComponent = ETModel.Game.Scene.GetComponent<WorldManagerComponent>();
+            if (worldManagerComponent == null)
+            {
+                worldManagerComponent = ETModel.Game.Scene.AddComponent<WorldManagerComponent>();
+            }
             WorldEntity worldEntity = ComponentFactory.Create<WorldEntity>();
+            Debug.Log(worldManagerComponent == null);
+            Debug.Log(worldEntity == null);
             worldManagerComponent.AddWorld(worldEntity);
             return worldEntity;
         }
